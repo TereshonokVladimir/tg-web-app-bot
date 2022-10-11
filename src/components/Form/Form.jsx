800551967f2c5b1d3e2e6914d123f57409572969
Vladimir <tereshonokvova@gmail.com>
2022-10-11 15:59:37 +0300

    db

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,5 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import './Form.css';
-import {useTelegram} from "../../hooks/useTelegram";
 
 const Form = () => {
     const [country, setCountry] = useState('');
