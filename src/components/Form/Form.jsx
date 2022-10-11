8d9f6fc3281211afca5229e1085a767bc9e6583c
Vladimir <tereshonokvova@gmail.com>
2022-10-11 15:38:21 +0300

    db

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,80 +1,78 @@
-import React, {useState, useEffect, useCallback } from 'react'
-import './Form.css'
+import React, {useCallback, useEffect, useState} from 'react';
+import './Form.css';
+import {useTelegram} from "../../hooks/useTelegram";
 
 const Form = () => {
-  const [country, setCountry] = useState('')
-  const [street, setStreet] = useState('')
-  const [subject, setSubject] = useState('physical')
- 
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const {tg} = useTelegram();
 
-  const onSendData = useCallback(() => {
-      const data = {
-        country,
-        street,
-        subject,
-      }
-      window.Telegram.WebApp.onSendData(JSON.stringify(data))
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
     }, [country, street, subject])
 
     useEffect(() => {
-      window.Telegram.WebApp.onEvent('mainButtonClicked', onSendData)
-    
-      return () => {
-        window.Telegram.WebApp.offEvent('mainButtonClicked', onSendData)
-      }
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
     }, [onSendData])
-    
-  useEffect(() => {
-    window.Telegram.WebApp.MainButton.setParams({
-      text: 'Send data'
-    })
-  }, [])
 
-  useEffect(() => {
-    if (!street || !country) {
-      window.Telegram.WebApp.MainButton.hide()
-    } else {
-      window.Telegram.WebApp.MainButton.show()
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        })
+    }, [])
+
+    useEffect(() => {
+        if(!street || !country) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, street])
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
     }
-  }, [])
-  
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value)
-  }
-  const onChangeStreet = (e) => {
-    setStreet(e.target.value)
-  }
-  const onChangeSubject = (e) => {
-    setSubject(e.target.value)
-  }
 
-  return (
-    <div className={'form'}>
-      <h3>Enter your data</h3>
-      <input 
-        className={'input'} 
-        type='text' 
-        placeholder={'Country'} 
-        value={country}
-        onChange={onChangeCountry}
-      />
-      <input 
-        className={'input'} 
-        type='text' 
-        placeholder={'Street'} 
-        value={street}
-        onChange={onChangeStreet}
-      />
-      <select 
-        value={subject} 
-        onChange={onChangeSubject} 
-        className='select'
-      >
-        <option value={'physical'}>Individual</option>
-        <option value={'legal'}>Legal entity</option>
-      </select>
-    </div>
-  )
-}
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
 
-export default Form
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
+    return (
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Улица'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Физ. лицо</option>
+                <option value={'legal'}>Юр. лицо</option>
+            </select>
+        </div>
+    );
+};
+
+export default Form;
